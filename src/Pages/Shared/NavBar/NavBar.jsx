7b43333f0c664fede6fd16efc5eff4a5540c9de4
Anika <anika.bd1994@gmail.com>
2023-06-09 23:45:05 +0600

    login form added

--- src/Pages/Shared/NavBar/NavBar.jsx
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -8,6 +8,7 @@ const NavBar = () => {
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/courses'>Courses</Link></li>
         <li><Link to='/dashboard'>Dashboard</Link></li>
+        <li><Link to='/login'>Login</Link></li>
        
         
 
