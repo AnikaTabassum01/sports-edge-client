6348a874054802b479926dc8544aa9eaded9f933
Anika <anika.bd1994@gmail.com>
2023-06-14 17:50:23 +0600

    some code done

--- src/Pages/Shared/ErrorPage/ErrorPage.jsx
+++ src/Pages/Shared/ErrorPage/ErrorPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 
 const ErrorPage = () => {
@@ -5,7 +6,7 @@ const ErrorPage = () => {
 
         <div className="ml-60">
             <img className="h-[500px]" src="https://i.pinimg.com/originals/ee/e6/49/eee6497eb3b97566443273b0485fbac5.gif" alt="" />
-            <button className="btn btn-warning mt-0 mb-4 normal-case text-xl text-white ml-60">Back to Home </button>
+           <Link to='/'> <button className="btn btn-warning mt-0 mb-4 normal-case text-xl text-white ml-60">Back to Home </button></Link>
         </div>
         
     );
